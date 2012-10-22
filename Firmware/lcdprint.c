7b643bcb39ad17bc26b07f062e723b843e3d3e83
Peter Volgyesi <peter.volgyesi@vanderbilt.edu>
2012-10-21 20:47:49 -0500

    Working A/V routines

--- Firmware/lcdprint.c
+++ Firmware/lcdprint.c
@@ -2,20 +2,21 @@
 #include <stdio.h>
 #include <stdint.h>
 
+// TODO: Rounding is not handled properly at the last digit (carry chain)
 
 #define AREF_LEVEL		2.56
 #define ADC_BITS        10
 #define MAX_SAMPLE  	((1<<ADC_BITS)-1)
-#define AVG_N_SAMPLES   1<<((sizeof(uint16_t)*8) - ADC_BITS)
+#define AVG_N_SAMPLES   (1<<((sizeof(uint16_t)*8) - ADC_BITS))
 #define MAX_A_SAMPLE    ((1<<(sizeof(uint16_t)*8))-1)
 
 #define V_POT	        1.8 // Nominal setting of the potentiometer
 #define V_R1            120.0
 #define V_R2            8.2
-#define V_A_S_10        (MAX_A_SAMPLE * (10.0 * (V_R2+V_POT) / (120+V_R2+V_POT)) / AREF_LEVEL)
-#define V_A_S_1         (MAX_A_SAMPLE * (1.0 * (V_R2+V_POT) / (120+V_R2+V_POT)) / AREF_LEVEL)
-#define V_A_S_0_1       (MAX_A_SAMPLE * (0.1 * (V_R2+V_POT) / (120+V_R2+V_POT)) / AREF_LEVEL)
-#define V_A_S_0_01      (MAX_A_SAMPLE * (0.01 * (V_R2+V_POT) / (120+V_R2+V_POT)) / AREF_LEVEL)
+#define V_A_S_10        (MAX_A_SAMPLE * (10.0 * (V_R2+V_POT) / (V_R1+V_R2+V_POT)) / AREF_LEVEL)
+#define V_A_S_1         (MAX_A_SAMPLE * (1.0 * (V_R2+V_POT) / (V_R1+V_R2+V_POT)) / AREF_LEVEL)
+#define V_A_S_0_1       (MAX_A_SAMPLE * (0.1 * (V_R2+V_POT) / (V_R1+V_R2+V_POT)) / AREF_LEVEL)
+#define V_A_S_0_01      (MAX_A_SAMPLE * (0.01 * (V_R2+V_POT) / (V_R1+V_R2+V_POT)) / AREF_LEVEL)
 
 #define A_GAIN          20.0
 #define A_POT           2.0 // Nominal setting of the potentiometer
@@ -27,6 +28,8 @@
 #define A_A_S_0_1       (MAX_A_SAMPLE * (0.1 * A_SENSE_R * (A_R2+A_POT) / (A_R1+A_R2+A_POT)) * A_GAIN / AREF_LEVEL)
 #define A_A_S_0_01      (MAX_A_SAMPLE * (0.01 * A_SENSE_R * (A_R2+A_POT) / (A_R1+A_R2+A_POT)) * A_GAIN / AREF_LEVEL)
 #define A_A_S_0_001     (MAX_A_SAMPLE * (0.001 * A_SENSE_R * (A_R2+A_POT) / (A_R1+A_R2+A_POT)) * A_GAIN / AREF_LEVEL)
+#define A_A_S_0_0001    (MAX_A_SAMPLE * (0.0001 * A_SENSE_R * (A_R2+A_POT) / (A_R1+A_R2+A_POT)) * A_GAIN / AREF_LEVEL)
+#define A_A_S_0_00001   (MAX_A_SAMPLE * (0.00001 * A_SENSE_R * (A_R2+A_POT) / (A_R1+A_R2+A_POT)) * A_GAIN / AREF_LEVEL)
 
 #define TEST_NOISE      0.01   // Maximum noise level (V), using uniform distribution (ideally, should be Gaussian)
 
@@ -38,7 +41,7 @@ uint16_t adc_v_read(double v)
     double v_noise;
     uint16_t sample;
     
-    v_noise = TEST_NOISE * ((2 * rand() / RAND_MAX) - 1);
+    v_noise = TEST_NOISE * ((2.0 * rand() / RAND_MAX) - 1.0);
     v_adc = (v * (V_R2+V_POT) / (120+V_R2+V_POT)) + v_noise;
     sample = MAX_SAMPLE *  v_adc / AREF_LEVEL;
     return sample;
@@ -50,7 +53,7 @@ uint16_t adc_a_read(double a)
     double v_noise;
     uint16_t sample;
 
-    v_noise = TEST_NOISE * ((2 * rand() / RAND_MAX) - 1);
+    v_noise = TEST_NOISE * ((2.0 * rand() / RAND_MAX) - 1.0);
     v_adc = ((a * A_SENSE_R) * ((A_R2 + A_POT)/(A_R2 + A_POT + A_R1))) + v_noise;    
     sample = MAX_SAMPLE * A_GAIN * v_adc / AREF_LEVEL;
     return sample;
@@ -69,7 +72,7 @@ void lcd_voltage(uint16_t sample, char* buff)
     
     d = r / V_A_S_10;
     if (d) {
-        buff[3] = '0' + d;  // 0 < d < 10, TODO: check & protect
+        buff[3] = '0' + d;
     } 
     else {
         buff[3] = ' ';
@@ -77,17 +80,17 @@ void lcd_voltage(uint16_t sample, char* buff)
     r -= d * V_A_S_10;
     
     d = r / V_A_S_1;
-    buff[4] = '0' + d;  // 0 <= d < 10, TODO: check & protect
+    buff[4] = '0' + d;
     r -= d * V_A_S_1;
 
     buff[5] = '.';
 
     d = r / V_A_S_0_1;
-    buff[6] = '0' + d;  // 0 <= d < 10, TODO: check & protect
+    buff[6] = '0' + d; 
     r -= d * V_A_S_0_1;
 
     d = r / V_A_S_0_01;
-    buff[7] = '0' + d;  // 0 <= d < 10, TODO: check & protect
+    buff[7] = '0' + d;
 
     buff[8] = ' ';
     buff[9] = 'V';
@@ -103,38 +106,75 @@ void lcd_ampere(uint16_t sample, char* buff)
     buff[1] = '=';
 
     if (sample < A_A_S_1) {
-        buff[6] = '\0';
+        uint8_t z = 1;
+        
+        if (sample < A_A_S_0_1) {
+            buff[2] = ' ';
+        }
+        else {
+            d = r / A_A_S_0_1;
+            buff[2] = '0' + d;
+            r -= d * A_A_S_0_1;
+            z = 0;
+        }
+        
+        if (z && (sample < A_A_S_0_01)) {
+            buff[3] = ' ';
+        }
+        else {
+            d = r / A_A_S_0_01;
+            buff[3] = '0' + d;
+            r -= d * A_A_S_0_01;
+        }
 
+        d = r / A_A_S_0_001;
+        buff[4] = '0' + d;
+        r -= d * A_A_S_0_001;
+        
+        buff[5] = '.';
+        
+        d = r / A_A_S_0_0001;
+        buff[6] = '0' + d;
+        r -= d * A_A_S_0_0001;
+
+        d = r / A_A_S_0_00001;
+        buff[7] = '0' + d;
+        r -= d * A_A_S_0_00001;
+
+
+        buff[8] = 'm';
     }
     else {
         buff[2] = ' ';
 
-        d = r / A_A_S_10;
-        if (d) {
-            buff[3] = '0' + d;  // 0 < d < 10, TODO: check & protect
-        } 
-        else {
+        if (sample < A_A_S_10) {
             buff[3] = ' ';
         }
-        r -= d * A_A_S_10;
+        else {
+            d = r / A_A_S_10;
+            buff[3] = '0' + d;
+            r -= d * A_A_S_10;
+        } 
         
         d = r / A_A_S_1;
-        buff[4] = '0' + d;  // 0 <= d < 10, TODO: check & protect
+        buff[4] = '0' + d;
         r -= d * A_A_S_1;
     
         buff[5] = '.';
     
         d = r / A_A_S_0_1;
-        buff[6] = '0' + d;  // 0 <= d < 10, TODO: check & protect
+        buff[6] = '0' + d; 
         r -= d * A_A_S_0_1;
     
         d = r / A_A_S_0_01;
-        buff[7] = '0' + d;  // 0 <= d < 10, TODO: check & protect
+        buff[7] = '0' + d; 
     
         buff[8] = ' ';
-        buff[9] = 'A';
-        buff[10] = '\0';
     }
+    
+    buff[9] = 'A';
+    buff[10] = '\0';
+
 }
 
 int main(int argc, char* argv[])
@@ -166,7 +206,7 @@ int main(int argc, char* argv[])
             sample_acc += (int)adc_a_read(a); 
         }
         lcd_ampere(sample_acc, buff);
-        double aa = sample_acc * (A_R1+A_R2+V_POT) / (A_R2+V_POT) * AREF_LEVEL / MAX_A_SAMPLE / A_GAIN / A_SENSE_R;
+        double aa = sample_acc * (A_R1+A_R2+A_POT) / (A_R2+A_POT) * AREF_LEVEL / MAX_A_SAMPLE / A_GAIN / A_SENSE_R;
         printf("%2.2f A: %2.2f A: %s [%5d]\n", a, aa, buff, (int)(sample_acc));
     }
     
